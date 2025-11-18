a4b1a613de7b734341f1a31ed6fae103e6555f42
Student-ysf <y.boudelaa.infg@lagh-univ.dz>
2025-11-18 08:44:31 +0100

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -3,66 +3,81 @@ import product2 from "../assets/dragonbaggyjoggerblue.jpg";
 import product3 from "../assets/dragonbaggyjoggerwhite.jpg";
 import product4 from "../assets/gymsharkbaggypant.jpg";
 import product5 from "../assets/gymsharkred.jpg";
+import product9 from "../assets/gymsharkwhite.jpg";
 import product6 from "../assets/spiderbaggyjoggerblue.jpg";
 import product7 from "../assets/spiderbaggyjoggerwhite.jpg";
 import product8 from "../assets/spiderbaggyjoggerred.jpg";
-
+import product10 from "../assets/Batman compressionshirtwhite.jpg";
 const products = [
   {
     id: 1,
     name: "Devil Wings Compression Shirt",
-    price: 2000,
+    price: 2300,
     images: [product1],
     description: "High-performance compression shirt with devil wings design, perfect for workouts.",
   },
   {
     id: 2,
     name: "Dragon Baggy Jogger Blue",
-    price: 2000,
+    price: 2200,
     images: [product2],
     description: "Comfortable blue baggy joggers with dragon print, ideal for street style and casual wear.",
   },
   {
     id: 3,
     name: "Dragon Baggy Jogger White",
-    price: 1800,
+    price: 2200,
     images: [product3],
     description: "White baggy joggers with dragon design, combining comfort and modern street fashion.",
   },
   {
     id: 4,
     name: "Gymshark Baggy Pant",
-    price: 2500,
+    price: 2200,
     images: [product4],
     description: "Black Gymshark baggy pants for training and casual looks, with breathable material.",
   },
   {
     id: 5,
-    name: "Gymshark Red Hoodie",
-    price: 2200,
+    name: "Gymshark Red",
+    price: 1800,
     images: [product5],
-    description: "Red hoodie from Gymshark, perfect for a sporty and urban outfit.",
+    description: "Red T-shirt from Gymshark, perfect for a sporty and urban outfit.",
   },
   {
     id: 6,
     name: "Spider Baggy Jogger Blue",
-    price: 1700,
+    price: 2500,
     images: [product6],
     description: "Blue baggy joggers with spider print, ideal for casual streetwear.",
   },
   {
     id: 7,
     name: "Spider Baggy Jogger White",
-    price: 2400,
+    price: 2500,
     images: [product7],
     description: "White baggy joggers featuring a spider design, stylish and comfortable for everyday wear.",
   },
   {
     id: 8,
     name: "Spider Baggy Jogger Red",
-    price: 1900,
+    price: 2500,
     images: [product8],
     description: "Red baggy joggers with spider motif, combining comfort and street fashion.",
+  },
+{
+    id: 9,
+    name: "Gym Shark White",
+    price: 1800,
+    images: [product9],
+    description: "White Gym Shark T-shirt designed for comfort and performance, featuring a clean athletic look and a premium soft fabric perfect for training or casual wear .",
+  },
+{
+    id: 10,
+    name: "Compression Shirt White",
+    price: 1900,
+    images: [product10],
+    description: "White compression shirt engineered for maximum support and flexibility, featuring breathable stretch fabric that enhances performance during workouts while keeping you cool and comfortable.",
   },
 ];
 
